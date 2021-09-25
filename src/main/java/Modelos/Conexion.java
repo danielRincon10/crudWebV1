a6a7a9942ce50a454c44806912c4efb0d8ba5e68
PC_Daniel <PC_Daniel@DESKTOP-V1DO5LS>
2021-09-24 19:35:51 -0500

    C2: Conexion y clase producto

---
+++ src/main/java/Modelos/Conexion.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+import java.sql.Connection; 
+import java.sql.DriverManager; 
+/**
+ *
+ * @author PC_Daniel
+ */
+public class Conexion {
+    //ATRIBUTOS
+    public Connection conn; 
+    public String host = "localhost:3306"; 
+    public String dataBase = "crudweb1"; 
+    public String user = "root"; 
+    public String password = ""; 
+    
+    //METODOS
+    public void conectar(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance(); 
+            conn = DriverManager.getConnection("jdbc:mysql://"+this.host+"/"+this.dataBase+"?zeroDateTimeBehavior=CONVERT_TO_NULL",this.user,this.password);
+            System.out.println("Conexión exitosa");
+        } catch (Exception error) {
+            System.out.println("Error de conexión: "+error);
+        }
+    }
+    
+    public void desconectar(){
+        conn = null; 
+    }
+}
--- src/main/java/Modelos/Producto.java
+++ src/main/java/Modelos/Producto.java
@@ -10,5 +10,76 @@ package Modelos;
  * @author PC_Daniel
  */
 public class Producto {
+    //ATRIBUTOS 
+    int codigoProducto; 
+    String nombreProducto; 
+    int cantidadProducto; 
+    int precioProducto; 
+    String categoriaProducto; 
+
+    //CONSTRUCTOR
+    public Producto() {
+    }
     
+    //ENCAPSULAR 
+    public int getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(int codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public int getCantidadProducto() {
+        return cantidadProducto;
+    }
+
+    public void setCantidadProducto(int cantidadProducto) {
+        this.cantidadProducto = cantidadProducto;
+    }
+
+    public int getPrecioProducto() {
+        return precioProducto;
+    }
+
+    public void setPrecioProducto(int precioProducto) {
+        this.precioProducto = precioProducto;
+    }
+
+    public String getCategoriaProducto() {
+        return categoriaProducto;
+    }
+
+    public void setCategoriaProducto(String categoriaProducto) {
+        this.categoriaProducto = categoriaProducto;
+    }
+    
+    //METODOS
+    public void crearProducto(){
+        
+    }
+    
+    public void consultarProducto(){
+        
+    }
+    
+    public void listarProducto(){
+        
+    }
+    
+    public void actualizarProducto(){
+        
+    }
+    
+    public void eliminarProducto(){
+        
+    }
 }
